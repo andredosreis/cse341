8253099b195c283d28e24e58b7689e6d40352385
Andre Dos Reis <andredosreis@gmail.com>
2025-02-27 20:20:06 +0000

    CSE341

---
+++ server.js
@@ -0,0 +1,14 @@
+// express server
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Andrew's Express Server!");
+});
+
+const port = 3000;
+ 
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
+ 
